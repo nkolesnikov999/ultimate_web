5ef8a6aa32fded390115c852e1604ce813fe4f8a
Nikolay Kolesnikov <nkolesnikov999@gmail.com>
2017-04-20 20:45:30 +0300

    Section 11 Swag Shop - WishList Component

--- Section11/swag-shop-web/src/App/App.js
+++ Section11/swag-shop-web/src/App/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+//Components
 import Product from '../product/product';
+import WishList from '../wishlist/wishlist';
+
+//Services
 import HttpService from '../services/http-service';
 
 const http = new HttpService();
@@ -31,9 +36,9 @@ class App extends Component {
     
     productList = () => {
         const list = this.state.products.map((product) => 
-            <div className="col-lg-4" key={product._id}>
-                <Product title={product.title} price={product.price} imgUrl={product.imgUrl}/>
-            </div>
+                <div className="col-sm-4" key={product._id}>
+                    <Product title={product.title} price={product.price} imgUrl={product.imgUrl}/>
+                </div>
         );
         
         return (list);
@@ -46,9 +51,16 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo" />
                     <h2>Welcome to The Swag Shop</h2>
                 </div>
-                <div className="container App-main">
+                <div className="container-fluid App-main">
                     <div className="row">
-                        {this.productList()}
+                        <div className="col-sm-8">
+                            <div className="row">
+                                {this.productList()}
+                            </div>
+                        </div>
+                        <div className="col-sm-4">
+                            <WishList />
+                        </div>
                     </div>
                     
                 </div>
---
+++ Section11/swag-shop-web/src/wishlist/wishlist.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import './wishlist.css';
+
+import ProductCondensed from '../product-condensed/product-condensed';
+
+class WishList extends Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {wishList:[
+            {
+                title:"Bologna Killer",
+                price:23.99,
+                _id:"sdfghjk"
+            },
+            {
+                title:"Foo Man Chu",
+                price:4.54,
+                _id:"zxcvb"
+            },
+            {
+                title:"Pipe Dream",
+                price:99.99,
+                _id:"sdfsdfghjk"
+            }
+        ]};
+        
+        //Bind functions
+        this.createWishList = this.createWishList.bind(this);
+    }
+    
+    createWishList = () => {
+        const list = this.state.wishList.map((product) => 
+            <ProductCondensed product={product} key={product._id} /> 
+        );
+        
+        return (list);
+    }
+    
+    render() {
+        return (
+            <div className="card">
+                <div className="card-block">
+                    <h4 className="card-title">Wish List</h4>
+                    <ul className="list-group">
+                        {this.createWishList()};
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default WishList;
