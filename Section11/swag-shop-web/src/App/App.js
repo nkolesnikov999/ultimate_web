bc3d3a68f7b4ba759fbff3547fa3d90e5a87743a
Nikolay Kolesnikov <nkolesnikov999@gmail.com>
2017-04-20 08:35:17 +0300

    Section 11 Swag Shop - Promises

--- Section11/swag-shop-web/src/App/App.js
+++ Section11/swag-shop-web/src/App/App.js
@@ -7,24 +7,37 @@ const http = new HttpService();
 
 class App extends Component {
     
-  constructor(props) {
-    super(props);
-    http.getProducts();
-  }    
+    constructor(props) {
+        super(props);
+        
+        //Bind functions
+        this.loadData = this.loadData.bind(this);
+        
+        this.loadData();
+    }  
     
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to The Swag Shop</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    loadData = () => {
+        http.getProducts().then(products => {
+            console.log(products);
+        }, err => {
+            console.log(err);
+        })
+    }
+    
+    
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to The Swag Shop</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
--- Section11/swag-shop-web/src/services/http-service.js
+++ Section11/swag-shop-web/src/services/http-service.js
@@ -2,10 +2,19 @@ import 'whatwg-fetch';
 
 class HttpService {
     getProducts = () => {
-        fetch('http://localhost:3004/product')
-        .then(response => {
-            console.log(response.json());
-        })
+        //1
+        var promise = new Promise((resolve, reject) => {
+            //2
+            fetch('http://localhost:3004/product')
+            .then(response => {
+                //4
+                resolve(response.json());
+                reject("You suck!");
+            })
+        });
+        
+        //3
+        return promise;
     }
 }
 
